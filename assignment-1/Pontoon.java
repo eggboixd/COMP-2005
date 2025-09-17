2f01b4880e23b5f1677804a21347fc1b1f92fa04
eggboixd <fadhil.revinno@gmail.com>
2025-09-17 20:10:28 -0200

    initial commit

---
+++ assignment-1/Pontoon.java
@@ -0,0 +1,11 @@
+
+public class Pontoon {
+    public static void main(String[] args) {
+        // JFrame frame = new JFrame("Pontoon Game");
+        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // frame.setSize(800, 600);
+        // frame.setVisible(true);
+
+        new MyFrame();
+    }
+}
